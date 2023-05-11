07363b678035e80e5e7e487ff52aff481a54d0ef
sphinx-austin <austinjuniour4@gmail.com>
2023-05-11 15:15:23 +0300

    user email verification update

--- base/emails.py
+++ base/emails.py
@@ -1,12 +1,11 @@
-from django.db import models
-import uuid
+import imp
+from django.conf import settings
+from django.core.mail import send_mail
 
 
-class BaseModel(models.Model):
-    uid = models.UUIDField(
-        primary_key=True, editable=False, default=uuid.uuid4)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        abstract = True
+def send_account_activation_email(email, email_token):
+    return
+    subject = 'Your account needs to be verified'
+    email_from = settings.EMAIL_HOST_USER
+    message = f'Hi, click on the link to activate your account http://127.0.0.1:8000/accounts/activate/{email_token}'
+    send_mail(subject, message, email_from, [email])
